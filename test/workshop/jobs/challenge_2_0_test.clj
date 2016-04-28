5e8a1bc3439cb412813e90d03d3bb2133105de3e
Yen-Chin Lee <coldnew.tw@gmail.com>
2016-04-28 16:07:12 +0800

    challenge 2.0
    Signed-off-by: Yen-Chin Lee <coldnew.tw@gmail.com>

--- test/workshop/jobs/challenge_2_0_test.clj
+++ test/workshop/jobs/challenge_2_0_test.clj
@@ -45,3 +45,5 @@
         (feedback-exception! peer-config job-id)
         (let [[results] (u/collect-outputs! lifecycles [:write-segments])]
           (u/segments-equal? expected-output results))))))
+
+;; (clojure.test/run-tests 'workshop.jobs.challenge-2-0-test)
